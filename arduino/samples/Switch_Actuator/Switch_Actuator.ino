36d09b458436d984f71879d1a2e57bf7df6b3455
thsvkd <thsvkd@snu.ac.kr>
2020-12-22 17:29:00 +0900

    small & big thing sample modify

--- arduino/samples/Switch_Actuator/Switch_Actuator.ino
+++ arduino/samples/Switch_Actuator/Switch_Actuator.ino
@@ -2,48 +2,65 @@
 #include <Servo.h>
 
 #define SERVO_PIN 8
+#define SERVO_PIN2 9
 
 #define CLIENT_NAME "SW_Act_1"
 
 #define SERVO_FUNCTION_ON "servo_switch_on"
 #define SERVO_FUNCTION_OFF "servo_switch_off"
 
+#define SW_STATUS "sw_status"
+
 Servo servo;
+Servo servo2;
 
 #if BOARD_SERIAL_IS_ONE
-ThingClient Client1(CLIENT_NAME, 3, Serial1);
+ThingClient Client1(CLIENT_NAME, 60, Serial1);
 #else
-ThingClient Client1(CLIENT_NAME, 3, Serial);
+ThingClient Client1(CLIENT_NAME, 60, Serial);
 #endif
+
+int sw_status;
 
 void servo_function_on(void *pData)
 {
+    servo2.attach(SERVO_PIN2);
     int power = 1, res;
-    int stop = 90, right_rotate = 200, left_rotate = 0;
+    int mid = 90, turn = 45;
 
     // res = GetIntArgumentByName(pData, ARG_SERVO_SWITCH, &power);
     // if (res == -1)
     //     return;
 
-    //TODO(thsvkd) - tuning servo.write Value to meetting room switch!!!
-    servo.write(left_rotate);
+    //TODO:(thsvkd) - tuning servo.write Value to meetting room switch!!!
+    servo2.write(mid - turn);
     delay(300);
-    servo.write(stop);
+    servo2.write(mid);
+    sw_status = 1;
+    servo2.detach();
 }
 
 void servo_function_off(void *pData)
 {
+    servo.attach(SERVO_PIN);
     int power = 1, res;
-    int stop = 90, right_rotate = 200, left_rotate = 0;
+    int mid = 90, turn = -45;
 
     // res = GetIntArgumentByName(pData, ARG_SERVO_SWITCH, &power);
     // if (res == -1)
     //     return;
 
-    //TODO(thsvkd) - tuning servo.write Value to meetting room switch!!!
-    servo.write(right_rotate);
+    //TODO:(thsvkd) - tuning servo.write Value to meetting room switch!!!
+    servo.write(mid - turn);
     delay(300);
-    servo.write(stop);
+    servo.write(mid);
+    sw_status = 0;
+    servo.detach();
+}
+
+int get_sw_status()
+{
+    return sw_status;
 }
 
 void init_pin()
@@ -55,6 +72,7 @@ void init_pin()
 #endif
 
     pinMode(SERVO_PIN, OUTPUT);
+    pinMode(SERVO_PIN2, OUTPUT);
     pinMode(LED_BUILTIN, OUTPUT);
 
     for (int i = 0; i < 4; i++)
@@ -69,10 +87,14 @@ void init_pin()
 void init_sensor()
 {
     servo.attach(SERVO_PIN);
+    servo2.attach(SERVO_PIN2);
 }
 
 void init_Value()
 {
+    static Value SW_status(SW_STATUS, get_sw_status, 0, 2, 1000);
+
+    Client1.Add(SW_status);
 }
 
 void init_Function()
