2c7771f16d0f6c27a49bdb2e836461b63cbd8cae
ikess <ikess0330@iris.snu.ac.kr>
2021-01-08 18:33:45 +0900

    Debug ARM Board Issue
    - uncommented pack(pop).
    - add light_sensor sample.
    
    Signed-off-by: ikess <ikess0330@iris.snu.ac.kr>

--- arduino/libraries/SoPIoT/sopiot.cpp
+++ arduino/libraries/SoPIoT/sopiot.cpp
@@ -1821,24 +1821,4 @@ void Thing::pubcompHandler(const msg_pubqos2 *msg) {
   response_wait_for_ = PUBLISH;
 }
 
-// thsvkd add(2021-01-06)
-void dp(const char *format, ...) {
-  va_list ap;
-  char buf[MAX_BUFFER_SIZE];
-
-  va_start(ap, format);
-  vsprintf(buf, format, ap);
-  va_end(ap);
-  Serial.print(buf);
-}
-
-void dlp(const char *format, ...) {
-  va_list ap;
-  char buf[MAX_BUFFER_SIZE];
-
-  va_start(ap, format);
-  vsprintf(buf, format, ap);
-  va_end(ap);
-  Serial.println(buf);
-}
 #endif
