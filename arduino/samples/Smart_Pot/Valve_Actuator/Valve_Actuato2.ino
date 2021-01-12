f395b25ebbc39d932d64b108f8e35681d5d237ca
jhsul51 <jhsul516@gmail.com>
2021-01-12 16:18:05 +0900

    Soil moisture sensor added
    - Soil moisture sensor added
    - Coding convention applied
    
    Signed-off-by: jhsul51 <jhsul516@gmail.com>

--- arduino/samples/Smart_Pot/Valve_Actuator/Valve_Actuato2.ino
+++ arduino/samples/Smart_Pot/Valve_Actuator/Valve_Actuato2.ino
@@ -1,99 +1,101 @@
-#include <thing_client.h>
+//----------------------------------------
+// Libraries
+//----------------------------------------
+
+//SoPIoT thing library
+#include <thing.h>
+
+// Module libraries
 #include <Servo.h>
 
-#define SERVO_PIN1 9
+//--------------------------------------------------
+// Modules
+// -------------------------------------------------
 
-Servo servo1; // servo1 is connected to pin9, and controls valve1
+// Modules
+Servo servo1;
+
+// Pins
+static const int kServo1Pin = 9;
 
 enum valve_status {
-    valve_close,
-    valve_open
+    valve_close = 0,
+    valve_open = 1
 };
 
-#if BOARD_SERIAL_IS_ONE
-ThingClient Client1("Valve_Act2", 60, Serial1);
-#else
-ThingClient Client1("Valve_Act2", 60, Serial);
-#endif
+//----------------------------------------
+// Thing
+//----------------------------------------
+// Thing(class_name, alive_cycle, serial);
+// Thing(class_name, serial);
+Thing valve_with_soil_moisture((const char *)"valve_with_soil_moisture", 60, SafeSerial);
 
-int valve1_status = 0;
+//----------------------------------------
+// Values
+// an SenseXXX overwrites a Value XXX
+//----------------------------------------
 
-void openValve1(void *pData)
+// Value variables
+int valve1_status_;
+int pot1_moisture_;
+
+// Getter functions of each Value variable
+int SenseValve1Status() { return valve1_status_; }
+int SensePot1Moisture() { return pot1_moisture_; }
+
+Value valve1_status((const char *)"valve1_status", SenseValve1Status, 0, 2, 
+                    10000);
+Value pot1_moisture((const char *)"pot1_status", SensePot1Status, 0, 2, 
+                    10000);
+
+//----------------------------------------
+// Functions
+// an ActuateXXX actuates a Function XXX
+//----------------------------------------
+
+void ActuateValve1Open(void *pData)
 {
-    servo1.attach(SERVO_PIN1);
     servo1.write(0);
-    delay(1000);
+    delay(300);
     valve1_status = valve_open;
-    servo1.detach();
 }
 
-void closeValve1(void *pData)
+void ActuateValve1Close(void *pData)
 {
-    servo1.attach(SERVO_PIN1);
     servo1.write(90);
-    delay(1000);
+    delay(300);
     valve1_status = valve_close;
-    servo1.detach();
 }
 
-int get_valve1_status()
-{
-    return valve1_status;
+Function valve1_open((const char *)"valve1_open", ActuateValve1Open, 0, 0);
+Function valve1_close((const char *)"valve1_close", ActuateValve1Close, 0, 0);
+
+void SetupSerial() { SafeSerial.begin(9600); }
+
+void SetupModules() {
+  // Setup Pin mode
+  pinMode(kServo1Pin, OUTPUT);
+
+  // Attach modules
+  servo1.attach(kServo1Pin);
 }
 
+void SetupThing() {
+    //Setup Functions
+    valve_with_soil_moisture.Add(ActuateValve1Open);
+    valve_with_soil_moisture.Add(ActuateValve1Close);
 
-void init_pin()
-{
-#if BOARD_SERIAL_IS_ONE
-    Serial1.begin(115200);
-#else
-    Serial.begin(115200);
-#endif
-
-    pinMode(SERVO_PIN1, OUTPUT);
-    pinMode(LED_BUILTIN, OUTPUT);
-
-    for (int i = 0; i < 4; i++)
-    {
-        digitalWrite(LED_BUILTIN, HIGH);
-        delay(100);
-        digitalWrite(LED_BUILTIN, LOW);
-        delay(100);
-    }
+    //Setup Values
+    valve_with_soil_moisture.Add(valve1_status);
+    valve_with_soil_moisture.Add(pot1_moisture);
+    //Setup Thing
+    valve_with_soil_moisture.Setup();
 }
 
-void init_sensor()
-{
-    servo1.attach(SERVO_PIN1);
+void setup() {
+  SetupSerial();
+  SetupModules();
+  SetupThing();
 }
 
-void init_Value()
-{
-    static Value VLV1_status("valve1_status", get_valve1_status, 0, 2, 1000);
-    Client1.Add(VLV1_status);
-}
-
-void init_Function()
-{
-    static Function fnOpenValve1("valve1_water_on", openValve1, 0, 0);
-    static Function fnCloseValve1("valve1_water_off", closeValve1, 0, 0);
-
-    Client1.Add(fnOpenValve1);
-    Client1.Add(fnCloseValve1);
-}
-
-void setup()
-{
-    init_pin();
-    init_sensor();
-
-    init_Value();
-    init_Function();
-
-    Client1.Setting();
-}
-
-void loop()
-{
-    Client1.DoLoop();
-}
+void loop() { valve_with_soil_moisture.Loop(); }
