2b2e7c20cd447e6feb4aa1813b83133af003f5b0
jhsul51 <jhsul516@gmail.com>
2021-01-12 16:14:54 +0900

    coding convention applied

--- arduino/samples/Smart_Pot/Valve_Actuator/Valve_Actuator.ino
+++ arduino/samples/Smart_Pot/Valve_Actuator/Valve_Actuator.ino
@@ -1,136 +1,126 @@
-#include <thing_client.h>
+//----------------------------------------
+// Libraries
+//----------------------------------------
+
+//SoPIoT thing library
+#include <thing.h>
+
+// Module libraries
 #include <Servo.h>
 
-#define SERVO_PIN1 9
-#define SERVO_PIN2 10
+//--------------------------------------------------
+// Modules
+// -------------------------------------------------
 
-Servo servo1; // servo1 is connected to pin9, and controls valve1
-Servo servo2; // servo2 is connected to pin10, and controls valve2
+// Modules
+Servo servo1;
+Servo servo2; 
+
+// Pins
+static const int kServo1Pin = 9;
+static const int kServo2Pin = 10;
 
 enum valve_status {
-    valve_close,
-    valve_open
+    valve_close = 0,
+    valve_open = 1
 };
 
-#if BOARD_SERIAL_IS_ONE
-ThingClient Client1("Valve_Act1", 60, Serial1);
-#else
-ThingClient Client1("Valve_Act1", 60, Serial);
-#endif
+//----------------------------------------
+// Thing
+//----------------------------------------
+// Thing(class_name, alive_cycle, serial);
+// Thing(class_name, serial);
+Thing valve_act1_thing((const char *)"SmartPotValve", 60, SafeSerial);
 
-int valve1_status = 0;
-int valve2_status = 0;
+//----------------------------------------
+// Values
+// an SenseXXX overwrites a Value XXX
+//----------------------------------------
 
-void openValve1(void *pData)
+// Value variables
+int valve1_status_;
+int valve2_status_;
+
+// Getter functions of each Value variable
+int SenseValve1Status() { return valve1_status_; }
+int SenseValve2Status() { return valve2_status_; }
+
+Value valve1_status((const char *)"valve1_status", SenseValve1Status, 0, 2, 
+                    10000);
+Value valve2_status((const char *)"valve2_status", SenseValve2Status, 0, 2, 
+                    10000);
+
+//----------------------------------------
+// Functions
+// an ActuateXXX actuates a Function XXX
+//----------------------------------------
+
+void ActuateValve1Open(void *pData)
 {
-    servo1.attach(SERVO_PIN1);
     servo1.write(0);
-    delay(1000);
+    delay(300);
     valve1_status = valve_open;
-    servo1.detach();
 }
 
-void closeValve1(void *pData)
+void ActuateValve1Close(void *pData)
 {
-    servo1.attach(SERVO_PIN1);
     servo1.write(90);
-    delay(1000);
+    delay(300);
     valve1_status = valve_close;
-    servo1.detach();
 }
 
-int get_valve1_status()
+void ActuateValve2Open(void *pData)
 {
-    return valve1_status;
-}
-
-
-
-void openValve2(void *pData)
-{
-    servo2.attach(SERVO_PIN2);
     servo2.write(0);
-    delay(1000);
+    delay(300);
     valve2_status = valve_open;
-    servo2.detach();
 }
 
-void closeValve2(void *pData)
+void ActuateValve2Close(void *pData)
 {
-    servo2.attach(SERVO_PIN2);
     servo2.write(90);
-    delay(1000);
+    delay(300);
     valve2_status = valve_close;
-    servo2.detach();
 }
 
-int get_valve2_status()
-{
-    return valve2_status;
+// Function declarations
+// Function(name, actuate_function, arguments_num, function_attributes_num);
+Function valve1_open((const char *)"valve1_open", ActuateValve1Open, 0, 0);
+Function valve1_close((const char *)"valve1_close", ActuateValve1Close, 0, 0);
+Function valve2_open((const char *)"valve2_open", ActuateValve2Open, 0, 0);
+Function valve2_close((const char *)"valve2_close", ActuateValve2Close, 0, 0);
+
+void SetupSerial() { SafeSerial.begin(9600); }
+
+void SetupModules() {
+  // Setup Pin mode
+  pinMode(kServo1Pin, OUTPUT);
+  pinMode(kServo2Pin, OUTPUT);
+
+  // Attach modules
+  servo1.attach(kServo1Pin);
+  servo2.attach(kServo2Pin);
 }
 
+void SetupThing() {
+    //Setup Functions
+    valve_act1_thing.Add(ActuateValve1Open);
+    valve_act1_thing.Add(ActuateValve1Close);
+    valve_act1_thing.Add(ActuateValve2Open);
+    valve_act1_thing.Add(ActuateValve2Close);
 
+    //Setup Values
+    valve_act1_thing.Add(valve1_status);
+    valve_act1_thing.Add(valve2_status);
 
-void init_pin()
-{
-#if BOARD_SERIAL_IS_ONE
-    Serial1.begin(115200);
-#else
-    Serial.begin(115200);
-#endif
-
-    pinMode(SERVO_PIN1, OUTPUT);
-    pinMode(SERVO_PIN2, OUTPUT);
-    pinMode(LED_BUILTIN, OUTPUT);
-
-    for (int i = 0; i < 4; i++)
-    {
-        digitalWrite(LED_BUILTIN, HIGH);
-        delay(100);
-        digitalWrite(LED_BUILTIN, LOW);
-        delay(100);
-    }
+    //Setup Thing
+    valve_act1_thing.Setup();
 }
 
-void init_sensor()
-{
-    servo1.attach(SERVO_PIN1);
-    servo2.attach(SERVO_PIN2);
+void setup() {
+  SetupSerial();
+  SetupModules();
+  SetupThing();
 }
 
-void init_Value()
-{
-    static Value VLV1_status("valve1_status", get_valve1_status, 0, 2, 1000);
-    static Value VLV2_status("valve2_status", get_valve2_status, 0, 2, 1000);
-    Client1.Add(VLV1_status);
-    Client1.Add(VLV2_status);
-}
-
-void init_Function()
-{
-    static Function fnOpenValve1("valve1_water_on", openValve1, 0, 0);
-    static Function fnCloseValve1("valve1_water_off", closeValve1, 0, 0);
-    static Function fnOpenValve2("valve2_water_on", openValve2, 0, 0);
-    static Function fnCloseValve2("valve2_water_off", closeValve2, 0, 0);
-
-    Client1.Add(fnOpenValve1);
-    Client1.Add(fnCloseValve1);
-    Client1.Add(fnOpenValve2);
-    Client1.Add(fnCloseValve2);
-}
-
-void setup()
-{
-    init_pin();
-    init_sensor();
-
-    init_Value();
-    init_Function();
-
-    Client1.Setting();
-}
-
-void loop()
-{
-    Client1.DoLoop();
-}
+void loop() { valve_act1_thing.Loop(); }
