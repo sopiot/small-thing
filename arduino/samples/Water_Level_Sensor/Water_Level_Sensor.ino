02b3def6bf3cc66c0e19e61052e227b4f7dc9d0b
thsvkd <thsvkd@snu.ac.kr>
2021-05-14 17:39:18 +0900

    Add WaterLevelSensor
    - Add WaterLevelSensor.ino
    
    Signed-off-by: thsvkd <thsvkd@snu.ac.kr>

--- arduino/samples/Water_Level_Sensor/Water_Level_Sensor.ino
+++ arduino/samples/Water_Level_Sensor/Water_Level_Sensor.ino
@@ -4,16 +4,15 @@
 
 // SoPIoT Thing library
 #include <thing.h>
+#define WATER_LEVEL_SENSOR_NUM 2
+
 // Module libraries
-
 // Pins
-static const int kWaterLevelPin = 13;
+const int kWaterLevelPin[WATER_LEVEL_SENSOR_NUM] = {2, 3};
 
 //----------------------------------------
 // Modules
 //----------------------------------------
-
-// Modules
 
 //----------------------------------------
 // Thing
@@ -28,47 +27,49 @@ Thing water_level_sensor((const char *)"WaterLevelSensor", 60, SafeSerial);
 // Values
 // an SenseXXX overwrites a Value XXX
 //----------------------------------------
-
 // Value variables
-int water_level_;
+int water_level_[2];
+double water_percentage_ = 0.0;
+double unit = 100.0 / WATER_LEVEL_SENSOR_NUM;
 
 // Getter functions of each Value variable
-int SenseWaterLevel() {
-  water_level_ = digitalRead(kWaterLevelPin);
-  return water_level_;
+int SenseWaterLevel() { 
+    double sum = 0;
+    for (int i=0; i < WATER_LEVEL_SENSOR_NUM; i++) {
+      water_level_[i] = digitalRead(kWaterLevelPin[i]);
+      sum += (water_level_[i] == 0) ? unit : 0.0;
+    }
+    return (int)sum;  
 }
 
 // Value declarations
 // Value(name, sense_function, min, max, period(ms));
-Value water_level((const char *)"water_level", SenseWaterLevel, 0, 1000, 1000);
+Value water_level((const char *)"water_level", SenseWaterLevel, 0, 100, 1000);
 
 //----------------------------------------
 // Functions
 // an ActuateXXX actuates a Function XXX
 //----------------------------------------
-
 // Function declarations
 // Function(name, actuate_function, arguments_num, function_attributes_num);
+
 
 //----------------------------------------
 // Setup
 //----------------------------------------
-
 void SetupSerial() { SafeSerial.begin(9600); }
 
 void SetupModules() {
   // Setup Pin mode
-  pinMode(kWaterLevelPin, INPUT);
-
+  pinMode(kWaterLevelPin[0], INPUT);
+  pinMode(kWaterLevelPin[1], INPUT);
   // Attach modules
 }
 
 void SetupThing() {
   // Setup Functions
-
   // Setup Values
   water_level_sensor.Add(water_level);
-
   // Setup Thing
   water_level_sensor.Setup();
 }
@@ -76,7 +77,6 @@ void SetupThing() {
 //----------------------------------------
 // Main
 //----------------------------------------
-
 void setup() {
   SetupSerial();
   SetupModules();
